bc46f99aa9ea104c42507c328b2442f719ff6025
sybz <wangxin19891226@hotmail.com>
2017-04-21 22:49:21 +0800

    Initial Commit of Library
    Signed-off-by: sybz <wangxin19891226@hotmail.com>

---
+++ podTestLibrary/Classes/Student.h
@@ -0,0 +1,15 @@
+//
+//  Student.h
+//  What
+//
+//  Created by wangsean on 2017/3/29.
+//  Copyright © 2017年 wangsean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+- (void)studentsFoo;
+
+@end
---
+++ podTestLibrary/Classes/Student.m
@@ -0,0 +1,18 @@
+//
+//  Student.m
+//  What
+//
+//  Created by wangsean on 2017/3/29.
+//  Copyright © 2017年 wangsean. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+- (void)studentsFoo
+{
+    NSLog(@"%s",__func__);
+}
+
+@end
